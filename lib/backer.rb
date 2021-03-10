bd6cc36eec58ee643aa2abb4482ef242223b3f39
Jaryd Hermann <hermannjaryd@gmail.com>
2021-03-10 12:04:19 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,9 +6,8 @@ def initialize(name)
   @backed_projects = []
 end
 
-def back_project=(project)
-  project.backer = self
-  @backed_projects << project
+def back_project=(Project)
+  @backed_projects << Project
 
 end
 
